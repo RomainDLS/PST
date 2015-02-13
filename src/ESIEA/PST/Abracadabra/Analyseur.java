06a5fe2a442e7e527fb8d9d3c222b2644dd64623
RomainDLS <delasouchere@et.esiea.fr>
2015-02-13 19:01:54 +0100

    merge additiond d'aurélien

---
+++ src/ESIEA/PST/Abracadabra/Analyseur.java
@@ -0,0 +1,53 @@
+package ESIEA.PST.Abracadabra;
+import javax.swing.*;
+
+import java.awt.*;
+
+public class Analyseur {
+	JFrame frame;
+	private Panel panel;
+	float[][] data;
+	
+	public Analyseur(float[][] data){
+		 this.data = new float[data.length][data[0].length];
+		 
+		 for(int i=0; i<data.length; i++){
+			 for(int j=0; j<data[i].length; j++){
+				 this.data[i][j] = data[i][j];
+			 }
+		 }
+		 frame = new JFrame();
+		 panel = new Panel();
+		 panel.setLayout(new FlowLayout());
+		 frame.add(panel);
+	     frame.setSize(600,600);
+		 frame.setVisible(true);
+		 
+		 printData(this.data);
+	}
+	
+	public void printData(float[][] data){
+		for(int i=0; i<data.length; i++){
+			 for(int j=0; j<data[i].length; j++){
+				 System.out.println(data[i][j]+ "\n");
+			 }
+		 }
+	}
+	
+	class Panel extends JPanel{
+		private static final long serialVersionUID = 1L;
+		
+		public void paintComponent(Graphics2D g){
+			for(int i = 0; i < data.length; i++) {
+			    for(int j = 0; j < data[i].length; j++) {
+			        double magnitude = Math.log(Math.abs(data[i][j]));
+			        
+			        g.setColor(new Color(0,(int)magnitude*10,(int)magnitude*20));
+			        g.fillRect(i*5,(data[i].length-j)*5,5,5);
+			    
+			    } 
+			}
+		}
+		
+	}
+}
--- src/fr/esiea/pst/abracadabra/Audio.java
+++ src/fr/esiea/pst/abracadabra/Audio.java
@@ -14,283 +14,42 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.sound.sampled.*;
+import it.sauronsoftware.jave.*;
 
-
-/*
-/***** Fonctions utiles pour la lecture d'autre fichiers ******/
-/*
-public class Audio extends Thread {
-
-	AudioInputStream audioInputStream = null;
-	SourceDataLine line;
-	
-
-public static long bytesToLongSignedLSB(byte[] frameBuffer, int start, int len) {
-
-
-    long l = 0;
-
-    if(frameBuffer.length > 0) {
-
-      int last = start+len-1;
-      l = frameBuffer[last] << (8 * last); //last byte remains signed
-      for(int r = len-2; r > 0; r--) {
-    	  
-        l += (frameBuffer[start + r] & 0xffL) << (8 * r);
-      }
-
-    }
-
-    return l;
-
-
-  }
-	
-
-/**
-   * LSB / Little endian case (WAV/RIFF)
-   */
-
-/*
-  public static long bytesToLongUnsignedLSB(byte[] frameBuffer, int start, int len) {
-
-
-    long l = 0;
-    for(int r = len-1; r >= 0; r--) {
-      l += (frameBuffer[start + r] & 0xffL) << (8 * r);
-    }
-    return l;
-
-
-  }
-	
-
-public static AudioFormat toSignedMonoPCM(AudioFormat sourceFormat) {
-
-
-    return new AudioFormat(Encoding.PCM_SIGNED, //tells whether samples are signed, forcing to PCM SIGNED for decoding (like most 16b PCM wavs, whereas 8bit PCM wavs are unsigned, and there can also be Float or µLaw wav's)
-
-                           sourceFormat.getSampleRate(),
-                           16, //bits per sample
-                           1, //mono
-                           2, //frame size (16/8)
-
-                           sourceFormat.getSampleRate(), //frame rate == sample rate in pcm
-                           false, //little endian in pcm
-                           sourceFormat.properties());
-  }
-
-
-  /**
-   * Useful when only one channel has to be analyzed
-
-  public static AudioInputStream getMonoInputStream(AudioInputStream ais) {
-
-    AudioFormat targetFormat = AudioUtils.toSignedMonoPCM(ais.getFormat());
-    return AudioSystem.getAudioInputStream(targetFormat, ais); // will return ais itself if the the in/out formats are the same
-
-
-  }
-   */
-
-//Version lecture d'un enregistrement audio
-// le programme enregistre dans un fichier wave un son, grâce à une interface graphique.
-// On click sur le boutton "start" pour commencer la capture et "stop" pour l'arrêter.
-// Une fois que le boutton "stop" à été actionné, le programme enregistre le fichier audio dans tableau (ici Byte)
-//On utilisera enfin Byte pour toutes sorte de transformation notamment grâces aux fonctions ci-dessus.
-
-
-
-public class Audio extends JFrame{
-	private static final long serialVersionUID = 1L;
-	AudioFormat audioFormat;
+public class Audio{
+    AudioFormat audioFormat;
     TargetDataLine targetDataLine;
     AudioInputStream audioInputStream;
 
-  final JButton captureBtn = new JButton("Capture");
-  final JButton stopBtn = new JButton("Stop");
-
-  public static void main( String args[]){
-    new Audio();
-    
-  }
-
-  public Audio(){
-    captureBtn.setEnabled(true);
-    stopBtn.setEnabled(false);
-
-    captureBtn.addActionListener(
-      new ActionListener(){
-        public void actionPerformed(ActionEvent e){
-          captureBtn.setEnabled(false);
-          stopBtn.setEnabled(true);
-          captureAudio();
-        }
-      }
-    );
-
-    stopBtn.addActionListener(
-      new ActionListener(){
-        public void actionPerformed(ActionEvent e){
-          captureBtn.setEnabled(true);
-          stopBtn.setEnabled(false);
-          targetDataLine.stop();
-          targetDataLine.close();
-        }
-      }
-    );
-    
-    getContentPane().add(captureBtn);
-    getContentPane().add(stopBtn);
-
-    getContentPane().setLayout(new FlowLayout());
-    setDefaultCloseOperation(EXIT_ON_CLOSE);
-    setSize(300,120);
-    setVisible(true);
-  }
-  
-  private void captureAudio(){
-    try{
-      audioFormat = getAudioFormat();	
-      System.out.println(audioFormat);
-      DataLine.Info dataLineInfo = new DataLine.Info(TargetDataLine.class,audioFormat);
-      targetDataLine = (TargetDataLine)
-      AudioSystem.getLine(dataLineInfo);
-
-      new CaptureThread().start();
-    }catch (Exception e) {
-      e.printStackTrace();
-      System.exit(0);
-    }
-  }
-
-  private AudioFormat getAudioFormat(){
-    float sampleRate = 8000.0F;
-    int sampleSizeInBits = 16;
-    int channels = 1;  
-    boolean signed = true;    
-    boolean bigEndian = false;
-   
-    return new AudioFormat(sampleRate,sampleSizeInBits,channels,signed,bigEndian);
-  }
-  
-class CaptureThread extends Thread{
-	
-	SourceDataLine line;
-	
-	
-  public void run(){
-    AudioFileFormat.Type fileType = AudioFileFormat.Type.WAVE;
-    File audioFile = new File("échantillon.wav");
-   
-    try{
-      targetDataLine.open(audioFormat);
-      targetDataLine.start();
-      AudioSystem.write(new AudioInputStream(targetDataLine),fileType,audioFile);
-      audioInputStream = AudioSystem.getAudioInputStream(audioFile);
-      System.out.println(audioInputStream);
-    }catch (Exception e){
-      e.printStackTrace();
+    public Audio(){
+    	this.audioFormat = null;
+    	this.targetDataLine = null;
+    	this.audioInputStream = null;
     }
     
+    public AudioFormat WAVFormat(){
+        float sampleRate = 44100;
+        int sampleSizeInBits = 8;
+        boolean signed = true;
+        int channels = 1;      
+        boolean bigEndian = false;
 
-	AudioFormat audioFormat = audioInputStream.getFormat();
-	DataLine.Info info = new DataLine.Info(SourceDataLine.class,
-			audioFormat);
+        return new AudioFormat(sampleRate, sampleSizeInBits, channels, signed, bigEndian);
+    }
     
-    try {
-		line = (SourceDataLine) AudioSystem.getLine(info);
-
-	} catch (LineUnavailableException e) {
-		e.printStackTrace();
-		return;
-	}
-
-	try {
-		line.open(audioFormat);
-	} catch (LineUnavailableException e) {
-		e.printStackTrace();
-		return;
-	}
-
-	line.start();
-    
-    try {
-		byte bytes[] = new byte[1024];
-		int bytesRead = 0;
-		int i = 0;
-
-		while ((bytesRead = audioInputStream.read(bytes, 0, bytes.length)) != -1) {
-			line.write(bytes, 0, bytesRead);
-			System.out.println("Byte =" + bytes[i]);
-
+    public File convertMP3toWAV(File mp3File){
+    	File wavFile = new File("Klingande.wav");
+    	AudioAttributes audio = new AudioAttributes();
+    	audio.setCodec("pcm_s16le");
+    	EncodingAttributes attrs = new EncodingAttributes();
+    	attrs.setFormat("wav");
+    	attrs.setAudioAttributes(audio);
+    	Encoder encoder = new Encoder();
+    	try {
+			encoder.encode(mp3File, wavFile, attrs);
+		} catch (IllegalArgumentException | EncoderException e) {
+			e.printStackTrace();
 		}
-	} catch (IOException io) {
-		io.printStackTrace();
-		return;
-	}
-    
-  }
+    	return wavFile;
+    }
 }
-
-}
-
- // Version lecture du fichier
-  
-  /*
-	public void run() {
-		File fichier = new File("échantillon.wav");
-		try {
-			AudioFileFormat format = AudioSystem.getAudioFileFormat(fichier);
-		} catch (UnsupportedAudioFileException e1) {
-			e1.printStackTrace();
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-
-		try {
-			audioInputStream = AudioSystem.getAudioInputStream(fichier);
-		} catch (UnsupportedAudioFileException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		AudioFormat audioFormat = audioInputStream.getFormat();
-		DataLine.Info info = new DataLine.Info(SourceDataLine.class,
-				audioFormat);
-
-		try {
-			line = (SourceDataLine) AudioSystem.getLine(info);
-
-		} catch (LineUnavailableException e) {
-			e.printStackTrace();
-			return;
-		}
-
-		try {
-			line.open(audioFormat);
-		} catch (LineUnavailableException e) {
-			e.printStackTrace();
-			return;
-		}
-
-		line.start();
-
-		try {
-			byte bytes[] = new byte[1024];
-			int bytesRead = 0;
-			int i = 0;
-
-			while ((bytesRead = audioInputStream.read(bytes, 0, bytes.length)) != -1) {
-				line.write(bytes, 0, bytesRead);
-				System.out.println("Byte =" + bytes[i]);
-
-			}
-		} catch (IOException io) {
-			io.printStackTrace();
-			return;
-		}
-
-	}
-}*/
