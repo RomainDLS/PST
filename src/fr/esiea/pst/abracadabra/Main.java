0fb59b059131faecb595178e36436d6e8eab8a3d
RomainDLS <delasouchere@et.esiea.fr>
2015-03-06 14:54:48 +0100

    Complex

--- src/fr/esiea/pst/abracadabra/Main.java
+++ src/fr/esiea/pst/abracadabra/Main.java
@@ -1,16 +1,28 @@
 package fr.esiea.pst.abracadabra;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.sound.sampled.UnsupportedAudioFileException;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		ImportToDb Import = new ImportToDb();
-	//	Audio son = new Audio();
+/*		ImportToDb Import = new ImportToDb();
 		
 		Import.SaveMusic("If 6'was\\9","ELdl","JH","Rock",1967,"NULL");
 		int id = Import.GetIdMusic("If 6'was\\9", "JH");
 		for(int i = 6; i<78;i++)
-			Import.AddSignature(id, i);
+			Import.AddSignature(id, i);*/
+		
+		File audiofile = new File("échantillon.wav");
+		try {
+			new FftProcessor().fft(audiofile);
+		} catch (UnsupportedAudioFileException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
